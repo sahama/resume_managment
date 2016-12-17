8321a5246c135cb45d345cb835f6a7cea7d3c9a2
Sayyid Hamid Mahdavi <s.h.mahdavi@chmail.ir>
2016-12-17 22:59:10 +0000

    اصلاحات جزئی در مدل

--- resume/models.py
+++ resume/models.py
@@ -9,15 +9,19 @@ class User(Document):
 
     @property
     def password(self):
-        return 'password should be hashed in database'
+        return 'password should be hashed in database and can not recover'
 
     @password.setter
     def password(self, new_password):
-        # TODO: using a method to hashing password in database
-        self._password = new_password
+        self._password = self.hash(new_password)
 
     def check_password(self, new_password):
-        return self._password == new_password
+        return self._password == self.hash(new_password)
+
+    @classmethod
+    def hash(cls, password):
+        # TODO: hash password geted as arg
+        return password
 
 
 
--- resume/views/home/views.py
+++ resume/views/home/views.py
@@ -3,10 +3,6 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates.jinja2')
 def my_view(context, request):
-    # print("context is", context)
-    # print("USer is", User)
-    # for u in User.objects:
-    #     print(u.email)
 
 
     return {'project': 'resume'}
