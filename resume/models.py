14d9d0970e2bb2573e00930136ff4c79f6d4de57
Sayyid Hamid Mahdavi <s.h.mahdavi@chmail.ir>
2016-12-20 02:55:44 +0000

    basic auth

--- resume/models.py
+++ resume/models.py
@@ -3,7 +3,6 @@ from mongoengine import *
 
 class User(Document):
     email = StringField(max_length=200, required=True)
-    # username = StringField(max_length=200, required=True)
     _password = StringField(max_length=200, required=True)
     mobile = StringField(max_length=200, required=True)
     groups = ListField(StringField(max_length=30))
